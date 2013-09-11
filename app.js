d0a36f7e3f4cdb82849250c4199ec95cd0a3ce2c
wilkie <wilkie05@gmail.com>
2013-09-11 16:48:35 -0400

    Sets initial value of iframe.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var textArea = document.getElementById("editor");
 var editBox = CodeMirror.fromTextArea(textArea, {
   lineNumbers: true,
-  value:       "<html>\n</html>",
   mode:        "text/html"
 });
+
+var iframe = document.getElementById("moo");
+iframe.src = "data:text/html;charset=utf-8," + escape(editBox.getValue());
 
 editBox.on("change", function(instance, object) {
   var iframe = document.getElementById("moo");
